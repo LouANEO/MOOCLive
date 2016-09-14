8409f23cf1ff72df09a498923ac65c9f8065b9ee
LouANEO <scharaudeau@aneo.fr>
2016-09-14 14:52:34 +0200

    Update subGraph.r

--- Mails/subGraph.r
+++ Mails/subGraph.r
@@ -1,22 +1,39 @@
-subGraph = function(folder, bornes)
+###################################################################################################################################
+###                                              Extract the backbone of the network                                              ###
+###################################################################################################################################
+
+#### Inputs ####
+# folder: path of the folder containing the data
+
+#### Outputs ####
+# edgesBackbone.txt and nodesBackbone.txt describing the backbone network, stocked in Network
+
+subGraph = function(folder)
 {
 	source(paste(folder,"/Network/backbone.r",sep=""))
-
+	
 	edges = read.table(paste(folder,"/Extraction/edges.txt",sep=""),sep="\t",header=TRUE)	
 	nodes = read.table(paste(folder,"/Extraction/nodes.txt",sep=""),sep="\t",header=TRUE)	
 
-	t = ponderation(folder, edges, bornes)
+	# A fixed value for the boundaries to perform data aggregation
+	boundaries = c(0,1,seq(1.2,4.4,0.2))
+
+	# Evaluate the relationship beyween edges weight and nodes number of posts
+	t = backbone(folder, edges, boundaries)
 
 	a = t$a
 	b = t$b
 	product = t$product
 
-	pondere = log(as.integer(as.matrix(edges[,3])))/(exp(log10(produit))*a+b)
+	# Evaluate the quotient between both values for each edge
+	weighting = log(as.integer(as.matrix(edges[,3])))/(exp(log10(produit))*a+b)
 
-	lim = quantile(pondere,probs=seq(0,1,0.1))[9]
+	# Select the most significantly frequent edges
+	lim = quantile(weighting,probs=seq(0,1,0.1))[9]
 	edges = edges[which(pondere>lim),]
 	nodes = nodes[which(!is.na(match(nodes[,1],unique(c(as.matrix(edges[,1]),as.matrix(edges[,2])))))),]
 	
-	write.table(edges, paste(folder,"/Network/edgesWeighted.txt",sep=""), sep="\t", col.names=TRUE, row.names=FALSE)
-	write.table(nodes, paste(folder,"/Network/nodesWeighted.txt",sep=""), sep="\t", col.names=TRUE, row.names=FALSE)
+	# Write the files for backbone network
+	write.table(edges, paste(folder,"/Network/edgesBackbone.txt",sep=""), sep="\t", col.names=TRUE, row.names=FALSE)
+	write.table(nodes, paste(folder,"/Network/nodesBackbone.txt",sep=""), sep="\t", col.names=TRUE, row.names=FALSE)
 }

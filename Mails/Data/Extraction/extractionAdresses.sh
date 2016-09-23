b0801a233e31b983ec692c0d095113e791519e82
LouANEO <scharaudeau@aneo.fr>
2016-09-23 12:35:06 +0200

    Add files via upload

---
+++ Mails/Data/Extraction/extractionAdresses.sh
@@ -0,0 +1,62 @@
+###################################################################################################################################
+###                               Extract sender and date of emission information from txt files                                ###
+###################################################################################################################################
+
+#### Inputs ####
+# Path of the folder containing the data
+
+#### Outputs ####
+# adresses.txt and dates.txt, stocked in Extraction, containing extracted data with the name of their file of origin
+
+#!/bin/bash
+
+rm $1/Extraction/*.txt
+
+# Going through each folder
+for folder in $(ls $1/TxtData/)
+do
+
+	# Writing the name of the files in a temporary file
+	ls $1/TxtData/$folder | awk '{gsub(/\.txt/,"");print $1,$5}'>> $1/Extraction/fichiersTemp2.txt
+	n=$(ls $1/TxtData/$folder -1 | wc -l)
+	yes $folder | head -n $n >> $1/Extraction/fichiersTemp.txt
+
+	paste $1/Extraction/fichiersTemp.txt $1/Extraction/fichiersTemp2.txt >> $1/Extraction/fichiers.txt
+
+	rm $1/Extraction/fichiersTemp.txt
+	rm $1/Extraction/fichiersTemp2.txt
+
+	# Going through the files in the current folder
+	for file in $(ls $1/TxtData/$folder)
+	do
+		# Extract the sender mail adress
+		resul=$(grep "From: " -m 1 $1/TxtData/$folder/$file | cut -d':'  -f2 | rev | cut -d' ' -f1 | rev | sed "s/<//g" | sed "s/>//g")
+
+		char=$(echo $resul | head -c 1)
+
+		if [ "$char" = "=" ]
+		then
+			resul=$(grep -A1 "From: " -m 1 $1/TxtData/$folder/$file | tail -1 | rev | cut -d' ' -f1 | rev | sed "s/<//g" | sed "s/>//g")
+		fi
+
+		echo $resul >> $1/Extraction/adressesTemp.txt
+
+		# Extract the sender mail adress
+		resul=$(grep "Date: " -m 1 $1/TxtData/$folder/$file)
+
+		prefix2="   Date: "
+		resul=${resul#$prefix2}
+
+                echo $resul >> $1/Extraction/datesTemp.txt
+	done
+
+done
+
+# Paste results
+paste $1/Extraction/fichiers.txt $1/Extraction/adressesTemp.txt >> $1/Extraction/adresses.txt
+paste $1/Extraction/fichiers.txt $1/Extraction/datesTemp.txt >> $1/Extraction/dates.txt
+
+# Supress temporary files
+rm $1/Extraction/adressesTemp.txt
+rm $1/Extraction/adresses2.txt
+rm $1/Extraction/datesTemp.txt
---
+++ Mails/Data/Extraction/merging.r
@@ -0,0 +1,61 @@
+###################################################################################################################################
+###                                                  Create a single data file                                                  ###
+###################################################################################################################################
+
+#### Inputs ####
+# folder: path of the folder containing data
+
+#### Outputs ####
+# A single data files containing all information in Extraction
+
+merging = function(folder)
+{
+	adresses = read.table(paste(folder,"/Extraction/adresses.txt",sep=""), sep="\t", header = FALSE)
+	dates = read.table(paste(folder,"/Extraction/dates.txt",sep=""), sep="\t", header = FALSE)
+	subject = read.table(paste(folder,"/Extraction/sub.txt",sep=""), sep="\t", header = FALSE)
+
+	month = read.table(paste(folder, "/DataMining/month.txt", sep=""), sep="\t", header=FALSE)
+	year = read.table(paste(folder, "/DataMining/year.txt", sep=""), sep="\t", header=FALSE)
+
+	# Create a single dataset containing all data
+
+	data = cbind(as.data.frame(adresses[,1:2]),as.data.frame(adresses[,3]),as.data.frame(dates[,3]),as.data.frame(subject[,3]))
+
+	# Suppress empty data or wrong format
+
+	data = data[data[,4]!="",]	
+	data = data[which(substr(data[,4],1,1)!="D"),]	
+
+	# Convert temporal data from JJ/MM/AAAA HH:MM:SS format to 6 columns
+
+	col <- data.frame(t(matrix(
+			unlist(strsplit(as.vector(data[,4]), split = " ")), 
+			ncol = length(data[,4]), nrow = 2)))
+
+	date1 <- data.frame(t(matrix(
+			unlist(strsplit(as.vector(col[,1]), split = "/")), 
+			ncol = length(col[,1]), nrow = 3)))
+	date2 <- data.frame(t(matrix(
+			unlist(strsplit(as.vector(col[,2]), split = ":")), 
+			ncol = length(col[,2]), nrow = 3)))
+	
+	# Reconstruction of the dataset
+
+	data = cbind(as.data.frame(data[,1:3]), as.data.frame(date1),as.data.frame(date2), as.data.frame(data[,5]))
+	colnames(data) = c("Folder", "File", "Sender", "Day", "Month", "Year", "Hour", "Minute", "Second", "Subject")
+
+	# Order datas by date of emission
+
+	YearInSecond = year[match(data$Year,year[,1]),2] * 24 * 60 *60
+	MonthInSecond = month[cbind(as.integer(data$Month),year[match(data$Year,year[,1]),3])] * 24 * 60 * 60
+
+	s = sort.int(as.integer(data$Second) + as.integer(data$Minute) * 60+ as.integer(data$Hour) * 60 * 60+ as.integer(data$Day) * 60 *60 * 24+ MonthInSecond+ YearInSecond,index.return=TRUE)[2]$ix
+
+	# Write the data in a file
+
+	write.table(data[s,],paste(folder,"/Extraction/data.txt",sep = ""),sep="\t",col.names=TRUE,row.names=FALSE)
+}
+
+args <- commandArgs()
+folder = args[6]
+merging(folder)
